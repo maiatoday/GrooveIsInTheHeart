e4a4810aa38332bc7c693c8bd604e401b422fdae
Maia Grotepass <maiatoday@gmail.com>
2023-04-17 19:34:40 +0200

    App icons

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,11 +1,14 @@
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
+    val icon = painterResource("groove.png")
     Window(
         title = "Groove is in the 💗",
+        icon = icon,
         state = rememberWindowState(width = 1200.dp, height = 850.dp),
         onCloseRequest = ::exitApplication
     ) {
