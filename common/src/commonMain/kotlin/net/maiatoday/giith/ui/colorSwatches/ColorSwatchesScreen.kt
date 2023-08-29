0c04d2d7e3b8867381f52e50af6629397667e980
Maia Grotepass <maiatoday@gmail.com>
2023-08-29 19:32:12 +0200

    versions and flowrow

--- common/src/commonMain/kotlin/net/maiatoday/giith/ui/colorSwatches/ColorSwatchesScreen.kt
+++ common/src/commonMain/kotlin/net/maiatoday/giith/ui/colorSwatches/ColorSwatchesScreen.kt
@@ -1,5 +1,8 @@
 package net.maiatoday.giith.ui.colorSwatches
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -11,18 +14,15 @@ import net.maiatoday.giith.ui.components.Pony
 import net.maiatoday.giith.ui.components.Troll
 import net.maiatoday.giith.ui.grooveColorNames
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun ColorSwatchesScreen() {
     Surface(modifier = Modifier.fillMaxSize()) {
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 172.dp)
-        ) {
+        FlowRow(verticalArrangement = Arrangement.Center) {
             for ((c, n) in grooveColorNames) {
-                item {
-                    Swatch(color = c, name = n)
-                }
-                if (n.contains("Troll")) item { Troll() }
-                if (n.contains("Pony")) item { Pony() }
+                Swatch(color = c, name = n)
+                if (n.contains("Troll")) Troll()
+                if (n.contains("Pony")) Pony()
             }
         }
     }
